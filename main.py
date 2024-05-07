0f087b536fc32abe82a4f360ef9c25523f3221a0
jacobkris <jacobkristians1@gmail.com>
2024-05-07 10:09:57 +0200

    add sample conversion functions and plot graphs

---
+++ main.py
@@ -0,0 +1,61 @@
+import numpy as np
+from scipy.interpolate import lagrange
+from scipy import signal
+import matplotlib.pyplot as plt
+import sample_conversion as sc
+
+plt.rcParams['axes.grid'] = True # set all grids to true
+
+wave_duration = 1
+sample_rate = 100
+f = 2
+decimate_factor = 4
+
+samples = wave_duration * sample_rate
+print("Number of samples: ", samples)
+decimate_samples = int(samples/decimate_factor)
+print("Number of decimated samples", decimate_samples)
+
+x = np.linspace(0,wave_duration, samples, endpoint=False)
+y = np.sin(x*np.pi*f*2)
+# print(type(y))
+
+y_down = sc.decimate(y,decimate_factor)
+x_down = np.linspace(0, wave_duration, decimate_samples,endpoint=False)
+# print("x downsampled:", x_down)
+# print("y downsampled:", y_down)
+
+interpolation_factor = 4
+y_padded = sc.zero_padding(y_down,interpolation_factor)
+x_padded = np.linspace(0,wave_duration,decimate_samples * interpolation_factor)
+
+# y_interpolated = sc.linear_interpolation(y_padded,interpolation_factor)
+y_fir_interpolated = sc.fir_lp_filter(y_padded,cutoff_freq=10)
+y_lagrange_interpolated = sc.lagrange_interpolation(y_padded,6)
+plt.rcParams['axes.grid'] = True
+
+y_optimal_interpolated = sc.optimal_finite_length_filt(y_padded,6)
+
+sc.lagrange_interpolation_new(y_down,0,4)
+
+
+# print("y padded: ",y_padded)
+# print("x padded: ",x_padded)
+# print("Num of samples ypadded",len(y_padded))
+# print("Num of samples xpadded",len(x_padded))
+
+fig, axs = plt.subplots(2,3)
+fig.suptitle("Upsampling methods")
+axs[0,0].plot(x,y,'.-') # original signal
+axs[0,0].set_title("Original signal")
+axs[0,1].plot(x_down,y_down,'.-') # downsampled signal
+axs[0,1].set_title("Downsampled signal")
+axs[0,2].plot(x_padded,y_padded,'.-') # zero padded signal
+axs[0,2].set_title("Zero padded signal")
+axs[1,0].plot(x_padded,y_fir_interpolated,'.-') # linear interpolated signal
+axs[1,0].set_title("FIR interpolated signal")
+axs[1,1].plot(x_padded,y_lagrange_interpolated,'.-') # lagrange interpolated signal
+axs[1,1].set_title("Lagrange interpolated signal")
+# axs[1,2].plot(x_padded,y_optimal_interpolated,'.-')
+# axs[1,2].set_title("Optimal interpolated signal")
+plt.show()
---
+++ sample_conversion.py
@@ -0,0 +1,71 @@
+import numpy as np
+from scipy import signal
+
+def decimate(x,decimation_factor):
+    y = signal.decimate(x,decimation_factor)
+    return y
+
+def zero_padding(x,interpolation_factor):
+    y_tmp = np.zeros(len(x)*interpolation_factor)
+
+    for i in range(0,len(x)):
+        y_tmp[i * interpolation_factor] = x[i]
+
+    y = y_tmp 
+    return y
+
+def linear_interpolation(x,interpolation_factor):
+    # y = [value1,0,0,0,value2,0,0,0,...]
+    y_tmp = []
+    for i in range(0,len(x),interpolation_factor):
+        y_tmp.append(x[i])
+        for j in range(1,interpolation_factor):
+            y_tmp.append(x[i + j])
+    y = y_tmp
+    return y 
+
+def fir_lp_filter(x, cutoff_freq):
+    # for loop?
+    num_taps = 8
+    nyquist_rate = 0.5 * 100 * 4
+    normalized_cutoff = cutoff_freq / nyquist_rate
+    taps = signal.firwin(num_taps,normalized_cutoff,window='hamming')
+    y = signal.filtfilt(taps,1.0,x)
+    
+    return y
+
+def lagrange_interpolation(x,order):
+    N = len(x)
+    x_interp = np.zeros(N)
+    
+    for n in range(N):
+        indices = np.arange(max(0, n - order), min(N, n + order + 1)) # nearby points
+        indices = indices[x[indices] != 0]  # take out zero
+        if len(indices) > 1: 
+            t = indices
+            y = x[indices]
+            
+            # calculate lagrange and sum for each
+            lagrange_basis = np.array([np.prod([(n - t[m]) / (t[j] - t[m]) for m in range(len(t)) if m != j]) for j in range(len(t))])
+            x_interp[n] = np.sum(y * lagrange_basis)
+        else:
+            x_interp[n] = x[n]  # copy if cannot interpolate
+    
+    return x_interp
+
+def lagrange_interpolation_new(x,order,interpolation_factor):
+    N = len(x)
+    x_interpolate = np.zeros(N * interpolation_factor)
+
+    for n in range(len(x_interpolate)):
+        break
+
+
+def optimal_finite_length_filt(x,order):
+    # 1. calculate filter coeffcients using optimal interpolator
+    optimal_coeff = x
+    # 2. apply the coefficients to a FIR filter
+    y = signal.lfilter(optimal_coeff, 1, x)
+
+    return y
+
