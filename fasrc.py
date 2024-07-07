5df6c5bcc81682c817d3ce7fbead7778ecf725ab
jacobkris <jacobkristians1@gmail.com>
2024-07-07 08:55:52 +0200

    working farrow filter, but delay do not work

--- fasrc.py
+++ fasrc.py
@@ -46,6 +46,7 @@ def plot_double(s1, s2, fs1, fs2, label1 = "Signal 1" , label2 = "Signal 2"):
     plt.legend()
     plt.show()
 
+
 def fir_precalculate_coeffs(fs_in,fs_out,cutoff,num_taps):
     """
     FIR filter coefficients
@@ -74,52 +75,55 @@ def lagrange_coeff(t, T, N):
                 coeff[i] *= (t - l * T) / (i * T - l * T)
     return coeff
 
-
-def fasrc(input_signal, fs_in, fs_out, N, num_taps=None):
+def design_farrow_coeffs(poly_order, num_subfilters , fc=0.4):
     """
-    fasrc is an flexible arbitrary ratio resampler that uses either Lagrange interpolation or fir filter for coefficient multiplying.
-    input_signal: input signal in int32
-    fs_in: input sampling rate
-    fs_out: output sampling rate
-    N: order of lagrange interpolation
-    num_taps: number of taps for fir filter (set to None for lagrange interpolation)
+    Farrow filter coefficients calculation
+    poly_order: polynomial order
+    num_subfilt: number of subfilters
     """
+    coeffs = [
+        0.0134723321567464, 0.0303640851789094, -0.00727985338412466, -0.0171107153161435, 0.0346995556841227, -0.0235535999197771, -0.0238593113883345, 0.0803821737365246, -0.0877592868647261, -0.0293147070318769, 0.564499995759284, 0.564499995759284, -0.0293147070318769, -0.0877592868647261, 0.0803821737365246, -0.0238593113883345, -0.0235535999197771, 0.0346995556841227, -0.0171107153161435, -0.00727985338412466, 0.0303640851789094, 0.0134723321567464
+    ]
+    p = np.polynomial.polynomial.Polynomial.fit(np.arange(0, len(coeffs)), coeffs, poly_order)
 
-    input_period = 1 / fs 
-    output_period = 1 / fs_new 
-    output_length = int(np.ceil(len(input_signal) * fs_out / fs_in))
+
+
+def fasrc(input_signal, input_rate, output_rate, farrow_coeffs=None):
+    # initialize coefficients
+    if farrow_coeffs is None:
+        farrow_coeffs = np.array([
+            [-8.57738278e-3, 7.82989032e-1, 7.19303539e+0, 6.90955718e+0, -2.62377450e+0, -6.85327127e-1, 1.44681608e+0, -8.79147907e-1, 7.82633997e-2, 1.91318985e-1, -1.88573400e-1, 6.91790782e-2, 3.07723786e-3, -6.74800912e-3],
+            [2.32448021e-1, 2.52624309e+0, 7.67543936e+0, -8.83951796e+0, -5.49838636e+0, 6.07298348e+0, -2.16053205e+0, -7.59142947e-1, 1.41269409e+0, -8.17735712e-1, 1.98119464e-1, 9.15904145e-2, -9.18092030e-2, 2.74136108e-2],
+            [-1.14183319e+0, 6.86126458e+0, -6.86015957e+0, -6.35135894e+0, 1.10745051e+1, -3.34847578e+0, -2.22405694e+0, 3.14374725e+0, -1.68249886e+0, 2.54083065e-1, 3.22275037e-1, -3.04794927e-1, 1.29393976e-1, -3.32026332e-2],
+            [1.67363115e+0, -2.93090391e+0, -1.13549165e+0, 5.65274939e+0, -3.60291782e+0, -6.20715544e-1, 2.06619782e+0, -1.42159644e+0, 3.75075865e-1, 1.88433333e-1, -2.64135123e-1, 1.47117661e-1, -4.71871047e-2, 1.24921920e-2]
+        ]) / 12.28
+
+    poly_order = farrow_coeffs.shape[0] - 1
+    num_subfilt = farrow_coeffs.shape[1]
+
+    ratio = output_rate / input_rate
+
+    output_length = int(np.ceil(len(input_signal) * ratio))
     output_signal = np.zeros(output_length)
 
     for sample_idx in range(output_length):
-        t_k = sample_idx * output_period # current time instant
-        n = int(t_k / input_period) # sample index for current time instant
-        delta_t = t_k - n * input_period # sampling interval
-        offset = 0
+        frac_delay = sample_idx / ratio
+        int_delay = int(frac_delay)
+        mu = frac_delay - int_delay
+        
+        for i in range(num_subfilt):
+            x = input_signal[int_delay - i] if int_delay - i >= 0 else 0
+            poly_value = 0
+            for j in range(poly_order + 1):
+                poly_value += farrow_coeffs[j][i] * (mu ** j)
+            output_signal[sample_idx] += x * poly_value
 
-        if num_taps is not None:
-            coeff = fir_precalculate_coeffs(fs_in, fs_out, 20000, num_taps)
-            N = num_taps
-            offset = num_taps // 2 # TODO
-        else:
-            coeff = lagrange_coeff(delta_t, input_period, N) # calculate lagrange coeffs
-
-        # pad input signal if necessary
-        if n + N > len(input_signal):
-            pad_length = n + N - len(input_signal)
-            input_signal = np.pad(input_signal, (0, pad_length), mode='edge')
-       
-        output_sample = 0
-        for i in range(N):
-            output_sample += coeff[i] * input_signal[n + i - offset]
-
-        output_signal[sample_idx] = output_sample # append sample to output signal
-    
-    return np.array(output_signal)
+    return output_signal
 
 if __name__ == "__main__":
-    # plot signal
-    f = 440 # 440 Hz
-    fs = 41100
+    
+    f = 6000 # 440 Hz
+    fs = 44100
     fs_new = 48000
     periods = 8
     duration = periods * 1/f 
@@ -133,26 +137,26 @@ if __name__ == "__main__":
     input_signal = 10000 * np.sin(2 * np.pi * f * t) 
     input_signal = input_signal.astype(np.int32)
     
-    real_output_signal = 10000 * np.sin(2 * np.pi * f * t_new)
-
-    output_signal_lagrange = fasrc(input_signal, fs, fs_new, 3)
-    output_signal_fir = fasrc(input_signal, fs, fs_new, 3, num_taps=16)
+    output_signal_ideal = 10000 * np.sin(2 * np.pi * f * t_new)
 
     output_signal_scipy = signal.resample(input_signal, n_new, domain='time')
 
+    output_signal_fasrc = fasrc(input_signal, fs, fs_new)
+    print("Input signal length: ", len(input_signal))
+    print("Output signal length: ", len(output_signal_ideal))
+    print("Output Farrow signal length: ", len(output_signal_fasrc))
 
-    plot_double(input_signal, output_signal_lagrange, fs, fs_new, label1="Input Signal", label2="Lagrange Interpolated Output")
-
-    plot_double(input_signal, output_signal_fir, fs, fs_new, label1="Input Signal", label2="FIR Interpolated Output")
-
-    plot_double(output_signal_lagrange, real_output_signal, fs_new, fs_new, label1="Lagrange Interpolated Output", label2="Real Interpolated")
-    
-    plot_double(output_signal_scipy, real_output_signal, fs_new, fs_new, label1="Scipy Interpolated Output", label2="Real Interpolated")
+    plot_double(input_signal, output_signal_fasrc, fs, fs_new, label1="Input Signal", label2="Farrow Interpolated Output")
+    plot_double(output_signal_ideal, output_signal_fasrc, fs_new, fs_new, label1="Ideal Output", label2="Farrow Interpolated Output")
+    plot_double(output_signal_ideal, output_signal_scipy, fs_new, fs_new, label1="Scipy Output", label2="Farrow Interpolated Output")
 
     # Mean squared error
-    mse_scipy = mse(output_signal_scipy, real_output_signal)
-    mse = mse(output_signal_lagrange, real_output_signal)
-    print("Mean squared error between real and my estimated: ", mse)
+    mse_fasrc = mse(output_signal_fasrc, output_signal_ideal)
+    mse_scipy = mse(output_signal_scipy, output_signal_ideal)
+    print("Mean squared error between real and my estimated: ", mse_fasrc)
     print("Mean squared error between real and scipy: ", mse_scipy)
 
-    # test mse of different interpolation orders
+    # Farrow coefficients
+    farrow_coeffs = design_farrow_coeffs(3, 4)
+    print(farrow_coeffs)
+
